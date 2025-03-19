34be751a369e601882dc6715d95bbc3ee7d250be
LykaOrbiso <lporbiso25@gmail.com>
2025-03-18 18:48:15 -0600

    Update

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 //this imports express
 const express = require('express');
-
 //use dotenv to expose our environment variables
 require('dotenv').config();
-
 //we create a new instance of express as the app object
 const app = express();
-
 //PORT assignment
 const PORT = process.env.PORT;
 
@@ -15,9 +12,14 @@ app.get('/home', (req, res) =>{
     res.send('Hello,World!');
 });
 
+//router handler that contains a path parameter
+app.post('/user/:id',(req, res) => {
+    const user_id = req.params.id;
+    res.send(`The user id is: ${user_id}`);
+});
+
 //start our server
 app.listen(PORT, () =>{
     console.log(`Server started on port: ${PORT}`);
-
 });
 
